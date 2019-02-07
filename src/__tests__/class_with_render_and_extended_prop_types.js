3a5ef3b11538e8ea01da28670893b7d60a3ba775
Daniele Esposti <daniele.esposti@gmail.com>
2019-02-07 17:16:09 +0000

    Added unit test with synthetic case

---
+++ src/__tests__/class_with_render_and_extended_prop_types.js
@@ -0,0 +1,31 @@
+const babel = require('babel-core');
+const content = `
+import React from 'react';
+import type { Props as BaseProps } from './types';
+
+type Props = BaseProps & { };
+
+class C extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render = () => {
+    return <div />;
+  };
+}
+
+`;
+
+it('class_with_render_and_extended_prop_types', () => {
+  const res = babel.transform(content, {
+    babelrc: false,
+    presets: ['@babel/env', '@babel/react', '@babel/flow'],
+    plugins: [
+      '@babel/syntax-flow',
+      require('../'),
+      "@babel/plugin-proposal-class-properties"
+    ],
+  }).code;
+  expect(res).toMatchSnapshot();
+});
